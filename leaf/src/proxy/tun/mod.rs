79f11f1a779a297b7681dd419fad32201508ea27
lulin <602814112@qq.com>
2025-07-07 00:39:35 +0800

    add smoltcp netstack as default stack

--- leaf/src/proxy/tun/mod.rs
+++ leaf/src/proxy/tun/mod.rs
@@ -1,3 +1,21 @@
-pub mod inbound;
 
+
+#[cfg(all(feature = "netstack-lwip", not(feature = "netstack-smoltcp")))]
 pub use netstack_lwip as netstack;
+
+#[cfg(all(feature = "netstack-smoltcp", not(feature = "netstack-lwip")))]
+pub use netstack_smoltcp as netstack;
+
+#[cfg(all(feature = "netstack-smoltcp", not(feature = "netstack-lwip")))]
+pub mod inbound_smoltcp;
+
+#[cfg(all(feature = "netstack-lwip", not(feature = "netstack-smoltcp")))]
+pub mod inbound_lwip;
+
+#[cfg(all(feature = "netstack-lwip", not(feature = "netstack-smoltcp")))]
+pub use self::inbound_lwip as inbound;
+
+#[cfg(all(feature = "netstack-smoltcp", not(feature = "netstack-lwip")))]
+pub use self::inbound_smoltcp as inbound;
+
+
